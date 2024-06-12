d20b9ff7b46ef509cc3ae3e4bf04859b2bf1babd
dhkswls1998 <dhkswls1998@naver.com>
2024-06-12 16:40:16 +0900

    for Deploy

--- src/index.js
+++ src/index.js
@@ -5,10 +5,13 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import {BrowserRouter} from "react-router-dom";
 
+// 환경변수를 사용해 개발환경(localhost) or 배포환경(krampoline) 구분
+const BASE_URL = process.env.REACT_APP_API_URL;
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <BrowserRouter>
-    <App />
+    <App baseUrl={BASE_URL} />
   </BrowserRouter>
 );
 
